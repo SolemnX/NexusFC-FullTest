d68c16c409e6a16acbecb39c8baeb979563e0ea7
SolemnX <solemnmessiah@gmail.com>
2024-08-29 16:22:57 -0400

    fix navigation

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -12,49 +12,49 @@ document.addEventListener('DOMContentLoaded', () => {
     if (ratesButton) {
         ratesButton.addEventListener('click', () => {
             console.log("Rates button clicked.");
-            window.location.href = 'htmls/rates.html';
+            window.location.href = 'rates.html';
         });
     }
 
     if (spawnButton) {
         spawnButton.addEventListener('click', () => {
             console.log("Spawn button clicked.");
-            window.location.href = 'htmls/spawn.html';
+            window.location.href = 'spawn.html';
         });
     }
 
     if (pokeButton) {
         pokeButton.addEventListener('click', () => {
             console.log("Poke button clicked.");
-            window.location.href = 'htmls/pokedata.html';
+            window.location.href = 'pokedata.html';
         });
     }
 
     if (mineButton) {
         mineButton.addEventListener('click', () => {
             console.log("Mine button clicked.");
-            window.location.href = 'htmls/miningindex.html';
+            window.location.href = 'miningindex.html';
         });
     }
 
     if (backButton) {
         backButton.addEventListener('click', () => {
             console.log("Back button clicked.");
-            window.location.href = 'htmls/index.html';
+            window.location.href = 'index.html';
         });
     }
 
     if (locButton) {
         locButton.addEventListener('click', () => {
             console.log("Location button clicked.");
-            window.location.href = 'htmls/location.html';
+            window.location.href = 'location.html';
         });
     }
 
     if (evlocButton) {
         evlocButton.addEventListener('click', () => {
             console.log("EV Location button clicked.");
-            window.location.href = 'htmls/evlocation.html';
+            window.location.href = 'evlocation.html';
         });
     }
 });
